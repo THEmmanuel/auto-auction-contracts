da22a84149d957782aa5ea4c4275adf3cd8d8bb6
Emmanuel <emmayodayo@gmail.com>
2023-10-21 00:58:31 +0100

    update auction.sol and nft.sol

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,69 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./NFT.sol";
-import "./Escrow.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Auction is Ownable {
-    NFT public nftContract;
-    Escrow public escrowContract;
-    uint256 public auctionCount;
-    enum AuctionStatus { Created, Open, Closed }
-    struct AuctionInfo {
-        address owner;
-        uint256 tokenId;
-        uint256 reservePrice;
-        uint256 endTime;
-        AuctionStatus status;
-    }
-    mapping(uint256 => AuctionInfo) public auctions;
+contract NFTAuction is Ownable {
+    ERC721 public nftContract;
+    IERC20 public paymentToken;
+    address public seller;
+    address public highestBidder;
+    uint256 public highestBid;
+    uint256 public tokenId;
+    bool public auctionEnded;
 
-    event AuctionCreated(uint256 auctionId, uint256 tokenId);
-    event AuctionEnded(uint256 auctionId, address winner);
+    event AuctionStarted(address indexed seller, uint256 tokenId);
+    event NewBid(address indexed bidder, uint256 amount);
+    event AuctionEnded(address indexed winner, uint256 amount);
 
-    constructor(address _nftAddress, address _escrowAddress) {
-        nftContract = NFT(_nftAddress);
-        escrowContract = Escrow(_escrowAddress);
-    }
-
-    function createAuction(uint256 tokenId, uint256 reservePrice, uint256 duration) external {
-        // Ensure the sender is the owner of the NFT.
-        require(nftContract.ownerOf(tokenId) == msg.sender, "Only NFT owner can create an auction");
-
-        uint256 auctionEndTime = block.timestamp + duration;
-        auctionCount++;
-
-        auctions[auctionCount] = AuctionInfo({
-            owner: msg.sender,
-            tokenId: tokenId,
-            reservePrice: reservePrice,
-            endTime: auctionEndTime,
-            status: AuctionStatus.Created
-        });
-
-        emit AuctionCreated(auctionCount, tokenId);
+    constructor(
+        address _nftAddress,
+        address _paymentTokenAddress,
+        uint256 _tokenId
+    ) {
+        nftContract = ERC721(_nftAddress);
+        paymentToken = IERC20(_paymentTokenAddress);
+        seller = msg.sender;
+        tokenId = _tokenId;
     }
 
-    function startAuction(uint256 auctionId) external onlyOwner {
-        AuctionInfo storage auction = auctions[auctionId];
-        require(auction.status == AuctionStatus.Created, "Auction is not in Created status");
-        require(auction.endTime > block.timestamp, "Auction has ended");
-
-        auction.status = AuctionStatus.Open;
+    function startAuction() external onlyOwner {
+        require(!auctionEnded, "Auction has already ended");
+        emit AuctionStarted(seller, tokenId);
     }
 
-    function endAuction(uint256 auctionId) external {
-        AuctionInfo storage auction = auctions[auctionId];
-        require(auction.status == AuctionStatus.Open, "Auction is not in Open status");
-        require(auction.endTime <= block.timestamp, "Auction is still open");
+    function placeBid(uint256 amount) external {
+        require(!auctionEnded, "Auction has ended");
+        require(msg.sender != seller, "Seller cannot bid");
+        require(amount > highestBid, "Bid must be higher than the current highest bid");
 
-        address winner = escrowContract.auctionToHighestBidder(auctionId);
-        require(winner != address(0), "No valid bids received");
+        // Refund the previous highest bidder.
+        if (highestBidder != address(0)) {
+            paymentToken.transfer(highestBidder, highestBid);
+        }
 
-        auction.status = AuctionStatus.Closed;
-        nftContract.safeTransferFrom(auction.owner, winner, auction.tokenId);
+        highestBidder = msg.sender;
+        highestBid = amount;
+        emit NewBid(msg.sender, amount);
+    }
 
-        emit AuctionEnded(auctionId, winner);
+    function endAuction() external onlyOwner {
+        require(!auctionEnded, "Auction has already ended");
+        auctionEnded = true;
+
+        // Transfer the NFT to the highest bidder.
+        nftContract.safeTransferFrom(seller, highestBidder, tokenId);
+
+        // Transfer the funds to the seller.
+        paymentToken.transfer(seller, highestBid);
+        emit AuctionEnded(highestBidder, highestBid);
+    }
+
+    function withdraw() external {
+        require(auctionEnded, "Auction has not ended yet");
+        require(msg.sender == seller || msg.sender == highestBidder, "You are not eligible to withdraw");
+
+        if (msg.sender == highestBidder) {
+            // Refund the bid amount to the highest bidder.
+            paymentToken.transfer(highestBidder, highestBid);
+        } else if (msg.sender == seller) {
+            // Withdraw any remaining funds to the seller.
+            uint256 remainingFunds = paymentToken.balanceOf(address(this));
+            if (remainingFunds > 0) {
+                paymentToken.transfer(seller, remainingFunds);
+            }
+        }
     }
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,9 +5,88 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721, Ownable {
-    constructor() ERC721("CarNFT", "CAR") {}
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
 
-    function mintNFT(address recipient, uint256 tokenId) external onlyOwner {
+    struct CarDetails {
+        string carImage;
+        string make;
+        string vin;
+        string transmission;
+        string model;
+        string engine;
+        string horsepower;
+    }
+
+    mapping(uint256 => CarDetails) public carInfo;
+
+    function mintNFT(
+        address recipient,
+        uint256 tokenId,
+        CarDetails memory details
+    ) external onlyOwner {
+        require(!_exists(tokenId), "NFT: Token ID already exists");
         _mint(recipient, tokenId);
+        carInfo[tokenId] = details;
+    }
+
+    function setCarDetails(
+        uint256 tokenId,
+        CarDetails memory details
+    ) external onlyOwner {
+        require(_exists(tokenId), "NFT: Token ID does not exist");
+        carInfo[tokenId] = details;
+    }
+
+    function getSVG(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "NFT: Token ID does not exist");
+        CarDetails memory details = carInfo[tokenId];
+        string memory svg = string(
+            abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="700" height="600">',
+                '<rect width="100%" height="100%" fill="#000" rx="10" ry="10" />',
+                '<image x="20" y="20" width="700" height="300" xlink:href="',
+                details.carImage,
+                '" />',
+                '<text x="120" y="390" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.make,
+                "</text>",
+                '<text x="340" y="390" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.vin,
+                "</text>",
+                '<text x="560" y="390" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.transmission,
+                "</text>",
+                '<text x="120" y="510" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.model,
+                "</text>",
+                '<text x="340" y="510" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.engine,
+                "</text>",
+                '<text x="560" y="510" font-size="15" fill="#FFF" text-anchor="middle">',
+                details.horsepower,
+                "</text>",
+                "</svg>"
+            )
+        );
+        return svg;
+    }
+
+    function safeTransferNFT(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external onlyOwner {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "NFT: Not approved or owner"
+        );
+        safeTransferFrom(from, to, tokenId);
+    }
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _exists(tokenId);
     }
 }
